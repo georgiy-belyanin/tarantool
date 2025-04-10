baffa7f82334788c57dde8984ada1c4451ade02b
Georgiy Belyanin <belyaningeorge@ya.ru>
2025-04-11 00:25:15 +0300

    config: allow guest to perform failover.execute
    This patch grants the guest user runtime privileges for running
    `failover.execute` function. This is an internal function used by the
    supervised failover coordinator. This privileges are needed for the
    guest user to be able to perform the initial bootstrap since there is no
    other users available before it.
    
    Technically it's a part of a native and supervised bootstrap strategy
    support allowing the external agent (i.e the supervised failover
    coordinator) to perform the initial bootstrap.
    
    Needed for tarantool/tarantool-ee#1194
    
    NO_CHANGELOG=internal change
    NO_TEST=will be tested in EE
    NO_DOC=internal change

--- src/box/lua/config/applier/runtime_priv.lua
+++ src/box/lua/config/applier/runtime_priv.lua
@@ -1,3 +1,5 @@
+local instance_config = require('internal.config.instance_config')
+
 -- Extract and add functions from a user or a role definition to
 -- the `{[func_name] = true, <...>}` mapping `res`.
 --
@@ -79,6 +81,25 @@ local function extract_funcs(user_name, ctx)
     return funcs
 end
 
+-- Grant other implicit privileges derived from configuration
+-- options other than in the `credentials` section.
+local function grant_implicit_privileges(configdata)
+    local iconfig_def = configdata._iconfig_def
+
+    -- For the bootstrap strategies involving the external
+    -- coordinator we need for the guest user to be able to
+    -- perform the initial bootstrap.
+    local bootstrap_strategy = instance_config:get(iconfig_def,
+        'replication.bootstrap_strategy')
+    local externally_managed_mode_strategies = {
+        ['supervised'] = true,
+        ['native'] = true,
+    }
+    if externally_managed_mode_strategies[bootstrap_strategy] then
+        box.internal.lua_call_runtime_priv_grant('guest', 'failover.execute')
+    end
+end
+
 local function apply(config_module)
     -- Prepare a context with the configuration information to
     -- transform.
@@ -117,6 +138,8 @@ local function apply(config_module)
             end
         end
     end
+
+    grant_implicit_privileges(configdata)
 end
 
 return {
